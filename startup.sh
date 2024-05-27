00ae237a2e55919cb2e8c00aadab17a240bcf8cf
Yue Tan <yuetan@cs.princeton.edu>
2024-05-27 00:30:03 -0400

    third attempt

--- startup.sh
+++ startup.sh
@@ -3,3 +3,8 @@
 apt update
 apt install -y ubuntu-drivers-common
 ubuntu-drivers install --gpgpu
+
+# query the installed NVIDIA driver version
+version=$(dpkg-query --show nvidia-kernel-common-* | awk '{print $1}' | awk -F'-' '{print $4"-" $5}')
+apt install -y nvidia-utils-$version
+
