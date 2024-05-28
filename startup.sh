93da71866a54acedb72b00f1e8e281668e880b21
Yue Tan <yuetan@cs.princeton.edu>
2024-05-28 17:15:32 -0400

    install pip and nvidia-cuda-toolkit

--- startup.sh
+++ startup.sh
@@ -11,6 +11,8 @@ ubuntu-drivers install --gpgpu
 
 # query the installed NVIDIA driver version
 version=$(dpkg-query --show nvidia-kernel-common-* | awk '{print $1}' | awk -F'-' '{print $4"-" $5}')
-apt install -y nvidia-utils-$version nvidia-driver-$version
+apt install -y nvidia-utils-$version nvidia-driver-$version nvidia-cuda-toolkit
+
+apt install -y python3-pip
 
 > /local/startup_service_done
